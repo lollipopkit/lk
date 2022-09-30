ba4fb5fb01f894502a9f78d18cead5d61a61f9ac
Junyuan Feng <lollipopkit@qq.com>
2022-09-30 14:25:08 +0800

    `REPL`支持方向键

--- go.mod
+++ go.mod
@@ -8,21 +8,16 @@ require (
 )
 
 require (
-	github.com/gdamore/encoding v1.0.0 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/rivo/uniseg v0.4.2 // indirect
 	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
-	golang.org/x/text v0.3.7 // indirect
 )
 
 require (
-	github.com/gdamore/tcell/v2 v2.5.3
+	atomicgo.dev/cursor v0.1.1
+	atomicgo.dev/keyboard v0.2.8
 	github.com/json-iterator/go v1.1.12
-	github.com/rivo/tview v0.0.0-20220916081518-2e69b7385a37
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
-	golang.org/x/term v0.0.0-20220919170432-7a66f970e087
 )
--- repl.go
+++ repl.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"os"
 
-	"git.lolli.tech/lollipopkit/go-lang-lk/consts"
-	"git.lolli.tech/lollipopkit/go-lang-lk/state"
+	"atomicgo.dev/cursor"
+	"atomicgo.dev/keyboard"
+	"atomicgo.dev/keyboard/keys"
+	"git.lolli.tech/lollipopkit/lk/consts"
+	"git.lolli.tech/lollipopkit/lk/state"
+)
+
+var (
+	linesHistory = []string{}
 )
 
 func repl() {
@@ -22,14 +28,81 @@ func repl() {
 
 	for {
 		os.Stdout.WriteString("> ")
-		ls.LoadString(readline(), "stdin")
-		
+
+		line := readline()
+		if line == "" {
+			continue
+		}
+
+		// 更新历史记录
+		updateHistory(line)
+
+		// 加载line，调用
+		ls.LoadString(line, "stdin")
 		ls.PCall(0, -1, 0)
 	}
 }
 
+func updateHistory(str string) {
+	idx := -1
+	for i := range linesHistory {
+		if linesHistory[i] == str {
+			idx = i
+			break
+		}
+	}
+	if idx != -1 {
+		linesHistory = append(linesHistory[:idx], linesHistory[idx+1:]...)
+	}
+	linesHistory = append(linesHistory, str)
+}
+
 func readline() string {
-	inputReader := bufio.NewReader(os.Stdin)
-    input, _ := inputReader.ReadString('\n')
-	return input
+	str := ""
+	linesIdx := len(linesHistory)
+
+	keyboard.Listen(func(key keys.Key) (stop bool, err error) {
+		switch key.Code {
+		case keys.CtrlC, keys.Escape:
+			os.Exit(0)
+		case keys.RuneKey:
+			runes := key.Runes
+			s := string(runes)
+			str += s
+			print(s)
+		case keys.Enter:
+			println()
+			return true, nil
+		case keys.Backspace:
+			if len(str) > 0 {
+				str = str[:len(str)-1]
+				resetLine(str)
+			}
+		case keys.Left:
+			cursor.Left(1)
+		case keys.Right:
+			cursor.Right(1)
+		case keys.Up:
+			if linesIdx > 0 {
+				linesIdx--
+				str = linesHistory[linesIdx]
+				resetLine(str)
+			}
+		case keys.Down:
+			if linesIdx < len(linesHistory)-1 {
+				linesIdx++
+				str = linesHistory[linesIdx]
+				resetLine(str)
+			}
+		}
+
+		return false, nil
+	})
+	return str
+}
+
+func resetLine(str string) {
+	cursor.ClearLine()
+	cursor.StartOfLine()
+	print("> " + str)
 }
